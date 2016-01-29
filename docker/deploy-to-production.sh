3c2dd0a58e6da0742d93cd6c2f9fbe0d412a4e5f
pbashyal-nmdp <pbashyal@nmdp.org>
2016-01-29 09:22:31 -0600

    prod deploy script

---
+++ docker/deploy-to-production.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PRODUCTION_MYSQL_HOST=productiondb.feature-service.com
+export PRODUCTION_MYSQL_PASSWORD=myproductionpassword
+
+docker-compose -p production -f docker-compose-prod.yml up -d
+
